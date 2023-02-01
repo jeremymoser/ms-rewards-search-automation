72276aeaaee41f57331ad181660a8d73b316d661
Jeremy Moser <jeremygmoser@icloud.com>
2023-02-01 11:00:34 -0500

    Adjusted keyboard key press to close browser on Windows.

--- main.py
+++ main.py
@@ -7,26 +7,29 @@ from wonderwords import RandomWord
 import webbrowser
 import keyboard
 
-# Initiate variables
-counter = 1
-searches = 30
+def perform_search():
+    # Initiate variables
+    counter = 1
+    searches = 2
 
-print("Today's searches include:")
-print("=========================")
+    print("Today's searches include:")
+    print("=========================")
 
-# While loop to iterate from counter to searches value
-while counter <= searches:
-    # Generate a random word utilizing RandomWord
-    rw = RandomWord()
-    unique_word = rw.word()
-    # Build URL and perform search using default browser
-    request_url = "https://www.bing.com/search?q=" + unique_word
-    webbrowser.open(request_url)
-    print("#" + str(i) + ": " + unique_word)
-    # Increment i
-    counter += 1
-    # Wait 3.5 seconds before continuing
-    time.sleep(3.5)
-# Close current browser
-keyboard.press_and_release("ctrl+shift+q")
-print("=========================")
+    # While loop to iterate from counter to searches value
+    while counter <= searches:
+        # Generate a random word utilizing RandomWord
+        rw = RandomWord()
+        unique_word = rw.word()
+        # Build URL and perform search using default browser
+        request_url = "https://www.bing.com/search?q=" + unique_word
+        webbrowser.open(request_url)
+        print("#" + str(counter) + ": " + unique_word)
+        # Increment i
+        counter += 1
+        # Wait 3.5 seconds before continuing
+        time.sleep(3.5)
+    # Close current browser
+    keyboard.press_and_release("ctrl+shift+w")
+    print("=========================")
+
+perform_search()
